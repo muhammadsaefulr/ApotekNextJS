ff01582c1aa169000c864ec7cc3af6aaeb36213e
saepulid <saepull.id@gmail.com>
2024-03-03 20:54:17 +0700

    Update No cache ManualDataFetch.tsx

--- app/dashboard/manualDataFetch.ts
+++ app/dashboard/manualDataFetch.ts
@@ -1,23 +1,43 @@
 const dataValueSupplier = async (): Promise<any> => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/main/produk/supplier`, {
-      cache: "no-store"
-  });
-  return res.json();
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/main/produk/supplier`,
+    {
+      cache: "no-store",
+    },
+  )
+  return res.json()
 }
 
 const dataValueUsers = async (): Promise<any> => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/user`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/user`, {
+    cache: "no-cache",
+  })
   return res.json()
 }
 
 const dataValueProduct = async (): Promise<any> => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/main/produk`)
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/main/produk`,
+    {
+      cache: "no-cache",
+    },
+  )
   return res.json()
 }
 
 const dataValueTransaksi = async (): Promise<any> => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/main/transaksi?tanggal=&view=day`)
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/main/transaksi?tanggal=&view=day`,
+    {
+      cache: "no-cache",
+    },
+  )
   return res.json()
 }
 
-export { dataValueSupplier, dataValueUsers, dataValueProduct, dataValueTransaksi };
+export {
+  dataValueSupplier,
+  dataValueUsers,
+  dataValueProduct,
+  dataValueTransaksi,
+}
